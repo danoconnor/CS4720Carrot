6158fbde3f70fb1e3b8e6a7f2965e8434c189f50
mcs8sc <mcs8sc@virginia.edu>
2014-04-17 12:29:08 -0400

    interface changes

--- gen/com/example/cs4720/R.java
+++ gen/com/example/cs4720/R.java
@@ -14,6 +14,10 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int lightblue=0x7f070000;
+        public static final int orange=0x7f070001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -25,74 +29,83 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int grid_button_image=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int androidblue=0x7f020000;
+        public static final int blue=0x7f020001;
+        public static final int bluesquare=0x7f020002;
+        public static final int credits128=0x7f020003;
+        public static final int front=0x7f020004;
+        public static final int grid_button_image=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int large128=0x7f020007;
+        public static final int onetwentyeight=0x7f020008;
     }
     public static final class id {
-        public static final int TextView02=0x7f09000f;
-        public static final int TextView03=0x7f090012;
-        public static final int TextView04=0x7f090015;
-        public static final int TextView05=0x7f090017;
-        public static final int action_settings=0x7f09003f;
-        public static final int addButton=0x7f09001e;
-        public static final int addClassButton=0x7f090008;
-        public static final int addTakenButton=0x7f090033;
-        public static final int button2=0x7f09003b;
-        public static final int classField=0x7f09001d;
-        public static final int confirmPW=0x7f090024;
-        public static final int courseNum=0x7f090006;
-        public static final int courseNumField=0x7f090009;
-        public static final int courseNumSpinner=0x7f090030;
-        public static final int createSubmitButton=0x7f090022;
-        public static final int createUserButton=0x7f090004;
-        public static final int currentSchedButton=0x7f090035;
-        public static final int departmentField=0x7f090007;
-        public static final int deptSpinner=0x7f09002f;
-        public static final int difficultRatingSeek=0x7f090011;
-        public static final int difficultRatingValue=0x7f090010;
-        public static final int errorField=0x7f09000b;
-        public static final int firstName=0x7f090026;
-        public static final int fulfillReqSpinner=0x7f09002d;
-        public static final int gradSpinner=0x7f09002c;
-        public static final int header=0x7f09001a;
-        public static final int header2=0x7f09000c;
-        public static final int instructorRatingSeek=0x7f09000e;
-        public static final int instructorRatingValue=0x7f09000d;
-        public static final int interestRatingSeek=0x7f090016;
-        public static final int interestRatingValue=0x7f090018;
-        public static final int lastName=0x7f090028;
-        public static final int listView=0x7f09001b;
-        public static final int loginButton=0x7f090003;
-        public static final int majorSpinner=0x7f09002a;
-        public static final int offeredClassesButton=0x7f090034;
-        public static final int planButton=0x7f090039;
-        public static final int profSpinner=0x7f090031;
-        public static final int progressBar1=0x7f090005;
-        public static final int pw1=0x7f090020;
-        public static final int pw2=0x7f090025;
-        public static final int rateButton=0x7f09003a;
-        public static final int rateClassButton=0x7f090032;
-        public static final int reqLinearLayout=0x7f090037;
-        public static final int reqScrollView=0x7f090036;
-        public static final int reqsGridView=0x7f090038;
-        public static final int seeDetailsButton=0x7f09002e;
-        public static final int submitRatingButton=0x7f090019;
-        public static final int textView1=0x7f09001c;
-        public static final int textView2=0x7f09000a;
-        public static final int textView3=0x7f090027;
-        public static final int textView4=0x7f090029;
-        public static final int textView5=0x7f09002b;
-        public static final int textview=0x7f090021;
-        public static final int timeRatingSeek=0x7f090014;
-        public static final int timeRatingValue=0x7f090013;
-        public static final int updateSubmitButton=0x7f09003c;
-        public static final int usernameField=0x7f09001f;
-        public static final int usernameLoginField=0x7f090002;
-        public static final int usernameTV=0x7f090001;
-        public static final int viewRatingHeader=0x7f090023;
-        public static final int viewRatingLinearLayout=0x7f09003e;
-        public static final int viewRatingScrollView=0x7f09003d;
-        public static final int welcomeHeader=0x7f090000;
+        public static final int TextView02=0x7f0a0010;
+        public static final int TextView03=0x7f0a0013;
+        public static final int TextView04=0x7f0a0016;
+        public static final int TextView05=0x7f0a0018;
+        public static final int action_settings=0x7f0a0041;
+        public static final int addButton=0x7f0a001f;
+        public static final int addClassButton=0x7f0a0009;
+        public static final int addTakenButton=0x7f0a0035;
+        public static final int button2=0x7f0a003e;
+        public static final int classField=0x7f0a0020;
+        public static final int confirmPW=0x7f0a0026;
+        public static final int courseNum=0x7f0a0007;
+        public static final int courseNumField=0x7f0a000a;
+        public static final int courseNumSpinner=0x7f0a0032;
+        public static final int createSubmitButton=0x7f0a0024;
+        public static final int createUserButton=0x7f0a0004;
+        public static final int currentSchedButton=0x7f0a0037;
+        public static final int departmentField=0x7f0a0008;
+        public static final int deptSpinner=0x7f0a0031;
+        public static final int difficultRatingSeek=0x7f0a0012;
+        public static final int difficultRatingValue=0x7f0a0011;
+        public static final int errorField=0x7f0a000c;
+        public static final int firstName=0x7f0a0028;
+        public static final int fulfillReqSpinner=0x7f0a002f;
+        public static final int gradSpinner=0x7f0a002e;
+        public static final int header=0x7f0a001b;
+        public static final int header2=0x7f0a000d;
+        public static final int imageView1=0x7f0a0001;
+        public static final int info=0x7f0a001c;
+        public static final int instructorRatingSeek=0x7f0a000f;
+        public static final int instructorRatingValue=0x7f0a000e;
+        public static final int interestRatingSeek=0x7f0a0017;
+        public static final int interestRatingValue=0x7f0a0019;
+        public static final int lastName=0x7f0a002a;
+        public static final int listView=0x7f0a001d;
+        public static final int loginButton=0x7f0a0003;
+        public static final int majorSpinner=0x7f0a002c;
+        public static final int offeredClassesButton=0x7f0a0036;
+        public static final int planButton=0x7f0a003b;
+        public static final int profSpinner=0x7f0a0033;
+        public static final int progressBar1=0x7f0a0006;
+        public static final int pw1=0x7f0a0022;
+        public static final int pw2=0x7f0a0027;
+        public static final int rateButton=0x7f0a003c;
+        public static final int rateClassButton=0x7f0a0034;
+        public static final int reqLinearLayout=0x7f0a0039;
+        public static final int reqScrollView=0x7f0a0038;
+        public static final int reqsGridView=0x7f0a003a;
+        public static final int seeDetailsButton=0x7f0a0030;
+        public static final int submitRatingButton=0x7f0a001a;
+        public static final int textView1=0x7f0a001e;
+        public static final int textView2=0x7f0a000b;
+        public static final int textView3=0x7f0a0029;
+        public static final int textView4=0x7f0a002b;
+        public static final int textView5=0x7f0a002d;
+        public static final int textview=0x7f0a0023;
+        public static final int timeRatingSeek=0x7f0a0015;
+        public static final int timeRatingValue=0x7f0a0014;
+        public static final int updateSubmitButton=0x7f0a003d;
+        public static final int usernameField=0x7f0a0021;
+        public static final int usernameLoginField=0x7f0a0002;
+        public static final int usernameTV=0x7f0a0000;
+        public static final int viewRatingHeader=0x7f0a0025;
+        public static final int viewRatingLinearLayout=0x7f0a0040;
+        public static final int viewRatingScrollView=0x7f0a003f;
+        public static final int welcomeHeader=0x7f0a0005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -111,7 +124,7 @@ public final class R {
         public static final int view_rating=0x7f03000d;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -140,10 +153,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- src/com/example/cs4720/MainActivity.java
+++ src/com/example/cs4720/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				setContentView(R.layout.pick_class_for_rating);
-				initializePickClassForRatingPage(username);
+				initializePickClassForRatingPage(username);	
 			}
 		});
 
